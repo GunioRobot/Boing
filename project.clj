602522c1c7bb22073e1259d135c7553ee195582e
Luc Préfontaine <lprefontaine@softaddicts.ca>
2011-05-12 17:01:11 -0400

    Upgraded version #

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject Boing "1.0"
+(defproject boing "1.1"
   :min-lein-version "1.5.1"
   :disable-implicit-clean false
   :java-source-path "src"
@@ -6,14 +6,13 @@
   :javac-target "1.5"
   :warn-on-reflection false
   :target-dir "target/"
-  :jar-name "boing.jar"
   :clean-non-project-classes true
   :manifest {"Built-By" "build-manager"
              "Specification-Title" "Boing library"
              "Specification-Version" "1.0"
              "Specification-Vendor" "SoftAddicts Inc."
              "Implementation-Title" "Boing library"
-             "Implementation-Version" 	"1.4.0"
+             "Implementation-Version" 	"1.1"
              "Implementation-Vendor" "SoftAddicts Inc."
              }
   :omit-source false
